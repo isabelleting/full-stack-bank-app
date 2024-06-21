42a47b8353ccbf94973c85c6b2ebdd1cb2874f3c
Lei Ting <144510802+isabelleting@users.noreply.github.com>
2024-06-20 21:53:33 -0700

    Add files

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.get('/account/google/:name/:email', function (req, res) {
 
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/dist/index.html'))
 });
 
